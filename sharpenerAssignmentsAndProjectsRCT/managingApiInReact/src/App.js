906c436298a983ad0f2a914ac42332d8d71e991c
akshay7745 <akshay7745@gmail.com>
2024-02-29 20:08:56 +0530

    Refetching the api for every 5 seconds

--- sharpenerAssignmentsAndProjectsRCT/managingApiInReact/src/App.js
+++ sharpenerAssignmentsAndProjectsRCT/managingApiInReact/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -6,12 +6,27 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [error, setError] = useState(null);
+  console.log(error, new Date().getSeconds());
+  useEffect(() => {
+    let intervalId = "";
+    if (error) {
+      intervalId = setInterval(() => {
+        getMovies();
+      }, 5000);
+    }
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [error]);
+  console.log(error);
   async function getMovies() {
     try {
       setIsLoading(true);
       const res = await fetch("https://swapi.dev/api/films");
-
+      if (!res.ok) {
+        throw new Error("Something went wrong ...Retrying!!!");
+      }
       const data = await res.json();
       const updatedMovies = data?.results.map((movie) => {
         const { episode_id, opening_crawl, release_date, title } = movie;
@@ -24,7 +39,10 @@ function App() {
       });
       setMovies(updatedMovies);
       setIsLoading(false);
+      setError(null);
     } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
       console.log(error);
     }
   }
@@ -34,8 +52,14 @@ function App() {
         <button onClick={getMovies}>Fetch Movies</button>
       </section>
       <section>
-        {isLoading && <h2>Loading Please wait...</h2>}
-        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && !error && <h2>Loading Please wait...</h2>}
+        {!isLoading && !error && <MoviesList movies={movies} />}
+        {error && (
+          <>
+            <h3>{error}</h3>
+            <button onClick={() => setError(null)}>cancle</button>
+          </>
+        )}
       </section>
     </React.Fragment>
   );
