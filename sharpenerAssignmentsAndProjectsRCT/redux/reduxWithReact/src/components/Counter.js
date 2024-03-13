5fe72c80a24bbcaecce07df622cf6792f4068d22
akshay7745 <akshay7745@gmail.com>
2024-03-13 09:09:30 +0530

    Integrated react with react-redux and added buttons to increase or decrease the store value by 5

--- sharpenerAssignmentsAndProjectsRCT/redux/reduxWithReact/src/components/Counter.js
+++ sharpenerAssignmentsAndProjectsRCT/redux/reduxWithReact/src/components/Counter.js
@@ -1,12 +1,26 @@
-import classes from './Counter.module.css';
-
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div>
+        <button onClick={incrementHandler}>Increase By 5</button>
+        <button onClick={decrementHandler}>Decrease By 5</button>
+      </div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- sharpenerAssignmentsAndProjectsRCT/redux/reduxWithReact/src/store/store.js
+++ sharpenerAssignmentsAndProjectsRCT/redux/reduxWithReact/src/store/store.js
@@ -3,19 +3,11 @@ import { createStore } from "redux";
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
     return {
-      counter: state.counter + 1,
+      counter: state.counter + 5,
     };
   } else if (action.type === "decrement") {
     return {
-      counter: state.counter - 1,
-    };
-  } else if (action.type === "incrementBy2") {
-    return {
-      counter: state.counter + 2,
-    };
-  } else if (action.type === "decrementBy2") {
-    return {
-      counter: state.counter - 2,
+      counter: state.counter - 5,
     };
   } else {
     return state;
@@ -23,15 +15,4 @@ const counterReducer = (state = { counter: 0 }, action) => {
 };
 const store = createStore(counterReducer);
 
-const counterSubscriber = () => {
-  const latestState = store.getState();
-  console.log(latestState);
-};
-
-store.subscribe(counterSubscriber);
-
-store.dispatch({ type: "increment" });
-store.dispatch({ type: "decrement" });
-store.dispatch({ type: "incrementBy2" });
-store.dispatch({ type: "decrementBy2" });
 export default store;
