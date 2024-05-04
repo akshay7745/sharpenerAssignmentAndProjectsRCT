263b22aa7e8390470b6ff5bb997c61c30653f26e
akshay7745 <akshay7745@gmail.com>
2024-05-04 10:00:28 +0530

    Added Link so that user can be navigated to the pages dynamically.

--- sharpenerAssignmentsAndProjectsRCT/nextJS/src/app/aboutus/page.jsx
+++ sharpenerAssignmentsAndProjectsRCT/nextJS/src/app/aboutus/page.jsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+
 export default function AboutUs() {
-  return <h3>About Us</h3>;
+  const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
+  return (
+    <ul>
+      {details.map((dev) => {
+        return (
+          <Link key={dev.id} href={`/aboutus/${dev.id}`}>
+            <li>{dev.name}</li>
+          </Link>
+        );
+      })}
+    </ul>
+  );
 }
