23d9523212f51eca7dd11f4d0dce175f31bbbcd9
akshay7745 <akshay7745@gmail.com>
2024-08-14 14:35:37 +0530

    Create server and handle response

--- backendDevelopement/helloNode/app.js
+++ backendDevelopement/helloNode/app.js
@@ -1,8 +1,40 @@
 
 const http = require("http");
 
+// When url = /home , return response ==> Welcome home
+// When url = /about, return response ==> Welcome to About Us page
+// When url =/node, return response ==> Welcome to my Node Js project
 const server = http.createServer((req,res)=>{
-    console.log("Akshhay");
+const url = req.url;
+switch(url){
+    case "/home":
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write("</html>");
+        res.write("<body><h1>Welcome home</h1></body>");
+        res.end();
+        break;
+    case "/about":
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write("</html>");
+        res.write("<body><h1>Welcome to About Us page</h1></body>");
+        res.end(); 
+        break;
+    case "/node":
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write("</html>");
+        res.write("<body><h1>Welcome to my Node JS project</h1></body>");
+        res.end();    
+        break;
+     default:
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write("</html>");
+        res.write("<body><h1>Welcome to any url</h1></body>");
+        res.end();   
+}
 });
 
 server.listen(4000);
