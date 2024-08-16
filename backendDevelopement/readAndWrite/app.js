2d8fecc498bdbe885c6986d74d83866f22416361
akshay7745 <akshay7745@gmail.com>
2024-08-16 13:34:42 +0530

    Added route in node js app also learned importing and exporting in node

--- backendDevelopement/readAndWrite/app.js
+++ backendDevelopement/readAndWrite/app.js
@@ -1,41 +1,6 @@
 const http = require("http");
-const fs = require("fs");
+const handler = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    fs.readFile("userData.txt", "utf8", (err, data) => {
-      if (err) {
-        data = "no data found";
-      }
-
-      res.write("<html>");
-      res.write("<head><title>Enter Message</title></head>");
-      res.write(
-        `<body><form action="/message" method="POST"><p>${data}</p> <input type="text" name="data"> <button type="submit">Send Data</button></form></body>`
-      );
-      res.write("</html>");
-      return res.end();
-    });
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("userData.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-});
+const server = http.createServer(handler);
 
 server.listen(4000);
