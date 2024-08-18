42a7b87cb772e17871c7b190b5e2e68d317d3d00
akshay7745 <akshay7745@gmail.com>
2024-08-18 09:10:58 +0530

    Used body parser for parsing the user data.

--- backendDevelopement/readAndWrite/app.js
+++ backendDevelopement/readAndWrite/app.js
@@ -1,14 +1,19 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware 1");
-  next();
-});
-app.use((req, res, next) => {
-  console.log("In the middleware 2");
-  res.send({ key1: "value" });
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use("/add-product",(req, res, next) => {
+res.send('<form action="/product" method="POST"><input placeholder="Enter product name" type="text" name="title"><input type="number" placeholder="Enter product size" name="size"><button type="submit">Send Data</button></form>');
 });
 
+app.post("/product",(req,res,next)=>{
+  console.log(req.body);
+  res.redirect("/");
+})
+
+app.use("/",(req,res,next)=>{
+  res.send('<h1>Hello from express</h1>')
+})
 app.listen(4000);
