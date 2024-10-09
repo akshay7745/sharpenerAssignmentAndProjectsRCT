de0c980809a4ec2d4447ae09d61feb90cb5b1dfd
akshay7745 <akshay7745@gmail.com>
2024-10-09 13:06:18 +0530

    implemented crud operations via my sql

--- backendDevelopement/dynamicRoutes/controllers/admin.js
+++ backendDevelopement/dynamicRoutes/controllers/admin.js
@@ -14,8 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect("/");
+  product
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -66,5 +70,10 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
+  Product.deleteById(prodId)
+    .then((data) => {
+      console.log("Product deleted successfully", data);
+    })
+    .catch((err) => console.log(err));
+  res.redirect("/admin/products");
 };
