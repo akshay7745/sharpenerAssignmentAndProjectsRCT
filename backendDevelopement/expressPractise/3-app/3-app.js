17114a28c0eaae1b847d2aa026a9197bc3efa755
akshay7745 <akshay7745@gmail.com>
2024-11-10 21:42:01 +0530

    added views and also provided public folder via static method

--- backendDevelopement/expressPractise/3-app/3-app.js
+++ backendDevelopement/expressPractise/3-app/3-app.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const http = require("http");
+// const http = require("http");
 const bodyParser = require("body-parser");
 const app = express();
-
+const path = require("path");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
+const successRoutes = require("./routes/success");
+const contactRoutes = require("./routes/contact");
+const rootDir = require("./utils/path");
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use("/shop", shopRoutes);
+app.use("/", contactRoutes);
+app.use("/success", successRoutes);
 
-app.use((req, res) => {
-  res.status(404).send("<h2>Page Not Found.</h2>");
+app.use("*", (req, res) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "not-found.html"));
 });
 
 app.listen(3000);
--- backendDevelopement/expressPractise/3-app/routes/contact.js
+++ backendDevelopement/expressPractise/3-app/routes/contact.js
@@ -1,12 +1,9 @@
 const express = require("express");
-
 const router = express.Router();
 const rootDir = require("../utils/path");
-
 const path = require("path");
-router.get("/", (req, res, next) => {
-  console.log("In the middleware number 2..");
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+router.get("/contactus", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "contactus.html"));
 });
 
 module.exports = router;
